b6dadaed2e1fb0283d2cec25e7ec00bffdde7363
anotherTrippFlattAcc <smakov2010qwerty@gmail.com>
2026-05-25 13:45:20 +0500

    Init

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="home-container">
+      <div className="background-overlay"></div>
+      
+      <main className="content">
+        <h1 className="title">
+          {"ИВЕНЬ"}
+        </h1>
+        
+        <p className="subtitle">
+          Добро пожаловать на официальный туториал по ивню. Здесь собраны все руководства по тактике, 
+          управлению и правилам проведения ивней. 
+          Изучи материалы перед игрой.
+        </p>
+
+        <div className="cta-group">
+          <button className="btn btn-primary" onClick={() => alert('Тут будет переход к туториалу!')}>
+            Открыть туториал
+          </button>
+          <button className="btn btn-secondary" onClick={() => alert('IP сервера скопирован!')}>
+            IP: play.iven.qzz.io
+          </button>
+        </div>
+      </main>
+
+      <footer className="footer">
+      </footer>
+    </div>
+  );
+}
+
+export default App
