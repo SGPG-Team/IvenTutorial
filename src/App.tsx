64fcf120e12d1cb349f6c690172c3c031d73ace2
anotherTrippFlattAcc <smakov2010qwerty@gmail.com>
2026-05-25 22:36:54 +0500

    ну нихуя себе

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function HomePage() {
         </p>
 
         <div className="cta-group">
-          <Link to="/wiki/rules" className="btn btn-primary">
+          <Link to="/wiki/stishok" className="btn btn-primary">
             Открыть вики
           </Link>
           <button className="btn btn-secondary" onClick={handleCopyIP}>
