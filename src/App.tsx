4beb15ac4f2c7db0b53c2e0a095d4d0118987287
anotherTrippFlattAcc <smakov2010qwerty@gmail.com>
2026-05-25 14:28:32 +0500

    копирование айпи

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+  const serverIP = "play.iven.qzz.io";
+
+  const [showToast, setShowToast] = useState(false);
+
   return (
     <div className="home-container">
       <div className="background-overlay"></div>
@@ -20,11 +25,25 @@ function App() {
           <button className="btn btn-primary" onClick={() => alert('Тут будет переход к туториалу!')}>
             Открыть вики
           </button>
-          <button className="btn btn-secondary" onClick={() => alert('IP сервера скопирован! (это пиздёж, не скопирован)')}>
-            IP: play.iven.qzz.io
+          <button className="btn btn-secondary" onClick={() => {
+            navigator.clipboard.writeText(serverIP);
+            setShowToast(true);
+
+            setTimeout(() => {
+              setShowToast(false);
+            }, 3000);
+          }}>
+            IP: {serverIP}
           </button>
         </div>
       </main>
+
+      <div className={`toast ${showToast ? 'toast-show' : ''}`}>
+        <span className="toast-icon">🖨️</span>
+        <div className="toast-text">
+          <h4>IP-адрес скопирован!</h4>
+        </div>
+      </div>
 
       <footer className="footer">
       </footer>
